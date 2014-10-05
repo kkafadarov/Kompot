afa6c0dc59a1de3c4b96eceb764569471c53a75d
si3792 <si3792@gmx.com>
2014-10-04 20:33:22 -0700

    UI added

--- KompotAndroid/app/src/main/java/com/jacobshack/kompotandroid/chooseAction.java
+++ KompotAndroid/app/src/main/java/com/jacobshack/kompotandroid/chooseAction.java
@@ -1,25 +1,24 @@
 package com.jacobshack.kompotandroid;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
 
-public class QRScannerActivity extends ActionBarActivity {
+
+public class chooseAction extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_qrscanner);
+        setContentView(R.layout.activity_choose_action);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.qrscanner, menu);
+        getMenuInflater().inflate(R.menu.choose_action, menu);
         return true;
     }
 
